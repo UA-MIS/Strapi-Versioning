93659be8de93832894703a291088f88eee62d013
rkharris2 <46576229+rkharris2@users.noreply.github.com>
2020-02-25 13:51:51 -0600

    Updated data calls to pull data from strapi

--- extensions/content-manager/services/ContentManager.js
+++ extensions/content-manager/services/ContentManager.js
@@ -31,18 +31,29 @@ module.exports = {
     );
   },
 
-  fetch(params, populate) {
+  async fetch(params, populate) {
     const { id, model } = params;
 
-    return strapi.entityService.findOne(
-      {
-        params: {
-          id,
-        },
-        populate,
-      },
-      { model }
-    );
+    var getParams = {
+      Bucket: process.env.AWS_BUCKET,
+      Key: 'newObject',
+      //VersionId: '8LmPm9ryal27CWDDU4pXV_KyKLFP9klP'
+  }
+
+    var dataReturned = await s3.getObject(getParams).promise();
+    dataReturned = JSON.parse(dataReturned.Body.toString())
+    console.log(dataReturned)
+    return dataReturned;
+    
+    // return strapi.entityService.findOne(
+    //   {
+    //     params: {
+    //       id,
+    //     },
+    //     populate,
+    //   },
+    //   { model }
+    // );
   },
 
   count(params, query) {
@@ -53,13 +64,12 @@ module.exports = {
   },
 
   create(data, { files, model } = {}) {
-    var bufferObject = new Buffer.from(JSON.stringify(data))
+    var bufferObject = new Buffer.from(JSON.stringify(data, {files, model}))
 
     var awsParams = {
       Bucket: process.env.AWS_BUCKET,
-      // Body: new Buffer(data, 'binary'),
       Body: bufferObject,
-      Key: 'NewRestaurantObject.json'
+      Key: data.ObjectName
     }
     s3.upload(awsParams, function (err, data2) {
       if (err) {
@@ -74,11 +84,42 @@ module.exports = {
   },
 
   edit(params, data, { model, files } = {}) {
+    var bufferObject = new Buffer.from(JSON.stringify(data, {files, model}))
+
+    var awsParams = {
+      Bucket: process.env.AWS_BUCKET,
+      Body: bufferObject,
+      Key: data.ObjectName
+    }
+    s3.upload(awsParams, function (err, data2) {
+      if (err) {
+        console.log("Error", err);
+      }
+
+      if (data2) {
+        console.log("Uploaded in:", data.location);
+      }
+    })
     return strapi.entityService.update({ params, data, files }, { model });
   },
 
   delete(params) {
     const { id, model } = params;
+
+    var getParams = {
+      Bucket: 'strapi.capstone.versioning',
+      Key: 'NewRestaurantObject.json',
+      //VersionId: '8LmPm9ryal27CWDDU4pXV_KyKLFP9klP'
+  }
+
+    s3.deleteObject(getParams, function(err, data) {
+      if (err) {
+      console.log(err, err.stack)
+      } else {
+      console.log(data)
+      }
+    })
+
     return strapi.entityService.delete({ params: { id } }, { model });
   },
 
