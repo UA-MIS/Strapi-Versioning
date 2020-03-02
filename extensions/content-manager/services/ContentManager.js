13b9f7cd1598fc1fce74eec0b3d7b50e87619108
rkharris2 <46576229+rkharris2@users.noreply.github.com>
2020-03-02 11:15:52 -0600

    Finished S3 Key naming conventions

--- extensions/content-manager/services/ContentManager.js
+++ extensions/content-manager/services/ContentManager.js
@@ -33,10 +33,10 @@ module.exports = {
 
   async fetch(params, populate) {
     const { id, model } = params;
-
+    var modelName = model.split(".")
     var getParams = {
       Bucket: process.env.AWS_BUCKET,
-      Key: params.id + '.JSON',
+      Key: modelName[1] + '/' + params.id + '.JSON',
       //VersionId: '8LmPm9ryal27CWDDU4pXV_KyKLFP9klP'
   }
 
@@ -66,6 +66,7 @@ module.exports = {
   async create(data, { files, model } = {}) {
     var bufferObject = new Buffer.from(JSON.stringify(data, {files, model}))
     var keyID;
+    var modelName = model.split(".")
     var dataCreated = await strapi.entityService.create({ data, files }, { model }).then(function(response){
       keyID = response.id
     });
@@ -73,7 +74,7 @@ module.exports = {
     var awsParams = {
       Bucket: process.env.AWS_BUCKET,
       Body: bufferObject,
-      Key: keyID + '.JSON'
+      Key: modelName[1] + '/' + keyID + '.JSON'
     }
     s3.upload(awsParams, function (err, data2) {
       if (err) {
@@ -89,11 +90,12 @@ module.exports = {
 
   edit(params, data, { model, files } = {}) {
     var bufferObject = new Buffer.from(JSON.stringify(data, {files, model}))
+    var modelName = model.split(".")
 
     var awsParams = {
       Bucket: process.env.AWS_BUCKET,
       Body: bufferObject,
-      Key: params.id + '.JSON'
+      Key: modelName[1] + '/' + params.id + '.JSON'
     }
     s3.upload(awsParams, function (err, data2) {
       if (err) {
@@ -109,10 +111,10 @@ module.exports = {
 
   delete(params) {
     const { id, model } = params;
-
+    var modelName = model.split(".")
     var getParams = {
       Bucket: process.env.AWS_BUCKET,
-      Key: params.id + '.JSON',
+      Key: modelName[1] + '/' + params.id + '.JSON',
   }
 
     s3.deleteObject(getParams, function(err, data) {
